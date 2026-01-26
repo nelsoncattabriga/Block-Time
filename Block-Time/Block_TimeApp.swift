5b050666ea06b6fca8670405b12c31933998de65
nelsoncattabriga <nelson@fastmail.fm>
2026-01-26 11:27:28 +1000

    Initial Commit

---
+++ Block-Time/Block_TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Block_TimeApp.swift
+//  Block-Time
+//
+//  Created by Nelson on 26/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct Block_TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
