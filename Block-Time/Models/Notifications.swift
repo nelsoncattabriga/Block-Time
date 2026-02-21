be4e33ce30965ae89db60316aa4778ccfeebb8a1
nelsoncattabriga <nelson@fastmail.fm>
2026-02-21 21:49:16 +1000

    Change of filename

---
+++ Block-Time/Models/Notifications.swift
@@ -0,0 +1,13 @@
+//
+//  Notifications.swift
+//  Block-Time
+//
+//  App-wide Notification.Name constants.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let flightDataChanged = Notification.Name("flightDataChanged")
+    static let scrollToTop       = Notification.Name("scrollToTop")
+}
